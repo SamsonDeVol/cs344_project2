ab7c8a0834472f006d830040506cac33ecb7e603
SamsonDeVol <devols@oregonstate.edu>
2022-01-15 19:28:22 -0800

    add functional readfile.c

---
+++ readfile.c
@@ -0,0 +1,32 @@
+// Samson DeVol, CS344 Project 2, 1/13/2022
+
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+int main (int argc, char *argv[]){
+
+  // open the file in read only mode
+  int fd= open(argv[1], O_RDONLY);
+
+  // perror if no file is open
+  if (fd == -1){
+    perror("open");
+  }
+
+  // set up buffer and read file
+  char buf[2048];
+  int count;
+
+  //read file and save to buffer
+  count = read(fd, buf, sizeof buf);
+
+  // write to standard output from buffer 
+  write(1, buf, count);
+
+  // close files
+  int close(int fd);
+  int close(int count);
+
+}
