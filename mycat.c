2718a279945a0f61866d729c7db6411e2dce1233
SamsonDeVol <devols@oregonstate.edu>
2022-01-16 20:27:55 -0800

    add mycat.c

--- mycat.c
+++ mycat.c
@@ -7,34 +7,30 @@
 
 int main(int argc, char *argv[]){
 
-char buf[2048];
-int count;
-printf("argc %d\n", argc);
-if (argc < 2){
-  int fd = open(0, O_RDONLY);
+  char buf[2048];
+  int count = 2;
 
-  printf("read = %zd\n", read(fd, buf, sizeof buf));
-  while (read(fd, buf, sizeof buf) > 0){
-    printf("read = %zd\n", read(fd, buf, sizeof buf));
-    count = read(fd, buf, sizeof buf);
-    write(1, buf, count);
+  if (argc < 2){
+    while (count > 1){
+      count = read(0, buf, sizeof buf);
+      write(1, buf, count);
+    }
   }
 
-}
-else{
-  for (int i = 1; i < argc; i++){
-    int fd = open(argv[i], O_RDONLY);
+  else{
+    for (int i = 1; i < argc; i++){
+      int fd = open(argv[i], O_RDONLY);
 
-    // perror if no file is open
-    if (fd == -1){
-      perror("open");
+      // perror if no file is open
+      if (fd == -1){
+        perror("open");
+      }
+
+      while (read(fd, buf, 1) > 0){
+        write(1, buf, 1);
+      }
+      
+      close(fd);
     }
-
-    while (read(fd, buf, 1) > 0){
-      write(1, buf, 1);
-    }
-
-    close(fd);
   }
-}
 }
